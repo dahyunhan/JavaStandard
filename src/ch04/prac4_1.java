44efc0634337ce11e59b1e2d5de7ce41453cbd8f
dollae <dollaeboss@gmail.com>
2022-06-26 21:13:02 +0900

    220626

---
+++ src/ch04/prac4_1.java
@@ -0,0 +1,14 @@
+package ch04;
+
+public class prac4_1 {
+    /*
+            1. x > 10 && x < 20
+            2. ch != " " || ch != "\t"
+            3. ch == 'x' || ch == 'X'
+            4. ch >= 0 && ch <=9
+            5. ch >= 'A' && ch <='z'
+            6. year % 400 == 0 && year % 4 == 0 && year % 100 != 0
+            7. !powerOn
+            8. str.equals("yes")
+     */
+}
