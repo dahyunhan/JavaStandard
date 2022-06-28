2cedefd73e93fbc5f5879d85030c80e522c0d7e2
dollae <dollaeboss@gmail.com>
2022-06-28 19:41:30 +0900

    220628

---
+++ src/ch04/Prac4_8.java
@@ -0,0 +1,10 @@
+package ch04;
+
+public class Prac4_8 {
+    public static void main(String[] args) {
+        //캐스팅
+        int value = (int) (Math.random() * 6 + 1);
+
+        System.out.println("value : " + value);
+    }
+}
