23b6ccc182d1a124374162b6438c0acc6df1719b
dollae <dollaeboss@gmail.com>
2022-07-16 14:43:24 +0900

    220716

---
+++ src/baekjoon/Timer2884.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Timer2884 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int h, m;
+        h = scanner.nextInt();
+        m = scanner.nextInt();
+
+        if (m < 45) {
+            if (h < 1) {
+                System.out.println((h - 1 + 24) + " " + (m + 15));
+
+            } else {
+
+                System.out.println(h - 1 + " " + (m + 15));
+            }
+        } else {
+            System.out.println(h + " " + (m + 15 - 60));
+        }
+    }
+}
